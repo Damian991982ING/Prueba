96443287f8dfe396e84c0d3e39586bb5dff846ac
Damian991982ING <103780729+Damian991982ING@users.noreply.github.com>
2022-04-16 01:53:20 -0300

    src

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Nabvar from './components/Navbar/Navbar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nabvar/>
+     
+    </div>
+  );
+}
+
+export default App;
